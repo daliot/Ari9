5b305748009141fdc0fe0ecf57749cff7ecbf6de
민규 최 <Min@TEAMBLACKCAT.local>
2012-02-09 21:51:58 +0900

    add vertical tab

--- project/Ari9/Ari9/FirstLayer.m
+++ project/Ari9/Ari9/FirstLayer.m
@@ -11,13 +11,68 @@
 
 @implementation FirstLayer
 
--(id) init
+- (id) initWithColor:(ccColor4B)color width:(GLfloat)w  height:(GLfloat) h
 {
-	// always call "super" init
+	
+		// always call "super" init
 	// Apple recommends to re-assign "self" with the "super's" return value
-	if( (self=[super init])) {
+	NSLog(@"111111111");
+	if( (self = [super initWithColor:color width:w height:h])) {
 		
-		NSLog(@"first Layer 생성");
+		CCMenuItem *first = [CCMenuItemFont itemWithString:@"[  1  ]" block:^(id sender) {
+			
+			//[self allTabLayersGoOffScreen];
+			//[self firstLayer].position = ccp(0,40);
+			//[self addChild:[self firstLayer]];
+			
+		}
+							 ];
+		
+		// Leaderboard Menu Item using blocks
+		CCMenuItem *second = [CCMenuItemFont itemWithString:@"[  2  ]" block:^(id sender) {
+			
+//			[self allTabLayersGoOffScreen];
+//			[self secondLayer].position = ccp(0,40);
+//			[self addChild:[self secondLayer]];
+			
+		}
+							  ];
+		
+		CCMenuItem *third = [CCMenuItemFont itemWithString:@"[  3  ]" block:^(id sender) {
+			
+//			[self allTabLayersGoOffScreen];
+//			[self thirdLayer].position = ccp(0,40);
+//			[self addChild:[self thirdLayer]];
+		}
+							 ];
+		
+		CCMenuItem *fourth = [CCMenuItemFont itemWithString:@"[  4  ]" block:^(id sender) {
+			
+//			[self allTabLayersGoOffScreen];
+//			[self fourthLayer].position = ccp(0,40);
+//			[self addChild:[self fourthLayer]];
+		}
+							  ];
+		
+		CCMenuItem *fifth = [CCMenuItemFont itemWithString:@"[  5  ]" block:^(id sender) {
+			
+//			[self allTabLayersGoOffScreen];
+//			[self fifthLayer].position = ccp(0,40);
+//			[self addChild:[self fifthLayer]];
+		}
+							 ];
+		
+		first.position = ccp(-130,170);
+		second.position = ccp(-130,120);
+		third.position = ccp(-130,70);
+		fourth.position = ccp(-130,20);
+		fifth.position = ccp(-130,-30);
+		
+		CCMenu *menu = [CCMenu menuWithItems:first, second,third,fourth,fifth, nil];
+		[menu setColor:ccRED];
+		
+		// Add the menu to the layer
+		[self addChild:menu];
 	}
 	return self;
 }
