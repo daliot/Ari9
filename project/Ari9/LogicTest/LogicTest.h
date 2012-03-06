14cb2c35b3b73418d84814cc881d7a9aba2f4cd3
daliot <tantara3@dreamwiz.com>
2012-03-06 20:15:39 +0900

    add LogicTest target

---
+++ project/Ari9/LogicTest/LogicTest.h
@@ -0,0 +1,13 @@
+//
+//  LogicTest.h
+//  LogicTest
+//
+//  Created by 오 화종 on 12. 03. 06.
+//  Copyright (c) 2012년 앱달. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface LogicTest : SenTestCase
+
+@end
---
+++ project/Ari9/LogicTest/LogicTest.m
@@ -0,0 +1,32 @@
+//
+//  LogicTest.m
+//  LogicTest
+//
+//  Created by 오 화종 on 12. 03. 06.
+//  Copyright (c) 2012년 앱달. All rights reserved.
+//
+
+#import "LogicTest.h"
+
+@implementation LogicTest
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in LogicTest");
+}
+
+@end
